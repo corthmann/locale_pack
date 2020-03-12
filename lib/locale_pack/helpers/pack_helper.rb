9be8d86f241e70fa0dd1b576154141709448b931
Karoly Szanto <karoly@vivino.com>
2020-03-12 17:36:30 +0100

    feat(LocalePack): read the content of the given locale pack file

--- lib/locale_pack/helpers/pack_helper.rb
+++ lib/locale_pack/helpers/pack_helper.rb
@@ -9,6 +9,16 @@ module LocalePack
       EOS
     end
 
+    # returns the locale pack's file content
+    def read_locale_pack(name, locale: nil)
+      path = "public#{locale_pack_path(name, locale: locale)}"
+
+      return unless File.file?(path)
+      locale_pack_content = File.read(path)
+
+      raw(locale_pack_content)
+    end
+
     def locale_pack_path(name, locale: nil)
       pack = LocalePack::Pack.find_by_name(name, locale: locale)
       pack.path
